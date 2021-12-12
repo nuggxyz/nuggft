d19d696ff020dc2c25320f0bdf62da74c1d27dd9
dub6ix <90335716+dub6ix@users.noreply.github.com>
2021-12-12 06:39:13 -0600

    adding tests back

---
+++ tests/hardhat/lib/fixtures/NuggFather.fix.ts
@@ -0,0 +1,81 @@
+import { Fixture, MockProvider } from 'ethereum-waffle';
+import { BigNumber, Wallet } from 'ethers';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+import { getHRE } from '../shared/deployment';
+import { deployContractWithSalt } from '../shared';
+import { NuggFT } from '../../../../typechain/NuggFT';
+import { NuggFT__factory } from '../../../../typechain/factories/NuggFT__factory';
+
+export interface NuggFatherFixture {
+    // nuggswap: NuggSwap;
+    // xnugg: xNUGG;
+    nuggft: NuggFT;
+
+    owner: string;
+    ownerStartBal: BigNumber;
+    hre: HardhatRuntimeEnvironment;
+    blockOffset: BigNumber;
+
+    // toNuggSwapTokenId(b: BigNumberish): BigNumber;
+}
+
+export const NuggFatherFix: Fixture<NuggFatherFixture> = async function (
+    wallets: Wallet[],
+    provider: MockProvider,
+): Promise<NuggFatherFixture> {
+    const hre = getHRE();
+
+    const eoaDeployer = provider.getWallets()[16];
+    const eoaOwner = provider.getWallets()[17];
+
+    // const xnugg = await deployContractWithSalt<xNUGG__factory>({
+    //     factory: 'xNUGG',
+    //     from: eoaDeployer,
+    //     args: [],
+    // });
+
+    // const nuggswap = await deployContractWithSalt<NuggSwap__factory>({
+    //     factory: 'NuggSwap',
+    //     from: eoaDeployer,
+    //     args: [xnugg.address],
+    // });
+
+    //0x435ccc2eaa41633658be26d804be5A01fEcC9337
+    //0x770f070388b13A597b84B557d6B8D1CD94Fc9925
+
+    const nuggft = await deployContractWithSalt<NuggFT__factory>({
+        factory: 'NuggFT',
+        from: eoaDeployer,
+        args: [eoaDeployer.address, eoaDeployer.address],
+    });
+
+    hre.tracer.nameTags[nuggft.address] = `NuggFT`;
+
+    const blockOffset = BigNumber.from(await hre.ethers.provider.getBlockNumber());
+
+    const owner = eoaDeployer.address;
+
+    // hre.tracer.nameTags[xnugg.address] = 'xNUGG';
+    // hre.tracer.nameTags[nuggswap.address] = 'NuggSwap';
+    hre.tracer.nameTags['0x0000000000000000000000000000000000000000'] = 'BLACK_HOLE';
+    hre.tracer.nameTags[owner] = 'Owner';
+    // hre.tracer.nameTags[mockERC1155.address] = 'mockERC1155';
+
+    // function toNuggSwapTokenId(epoch: BigNumberish): BigNumber {
+    //     return ethers.BigNumber.from(nuggswap.address).shl(96).add(epoch);
+    // }
+
+    return {
+        // toNuggSwapTokenId,
+        // mockERC1155,
+        // dotnugg: hre.dotnugg,
+        nuggft,
+        // xnugg,
+        blockOffset,
+        owner,
+        // nuggswap,
+        hre: getHRE(),
+        ownerStartBal: await getHRE().ethers.provider.getBalance(owner),
+    };
+};
---
+++ tests/hardhat/src/System.test.ts
@@ -0,0 +1,80 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
+import { ethers, waffle } from 'hardhat';
+
+import { NamedAccounts } from '../../../hardhat.config';
+import { Mining } from '../lib/shared/mining';
+import { prepareAccounts } from '../lib/shared';
+import { toEth } from '../lib/shared/conversion';
+import { NuggFatherFix, NuggFatherFixture } from '../lib/fixtures/NuggFather.fix';
+
+const createFixtureLoader = waffle.createFixtureLoader;
+const {
+    constants: { MaxUint256 },
+} = ethers;
+
+let loadFixture: ReturnType<typeof createFixtureLoader>;
+let accounts: Record<keyof typeof NamedAccounts, SignerWithAddress>;
+let fix: NuggFatherFixture;
+
+const refresh = async () => {
+    accounts = await prepareAccounts();
+    loadFixture = createFixtureLoader();
+    fix = await loadFixture(NuggFatherFix);
+};
+
+describe('uint tests', async function () {
+    beforeEach(async () => {
+        await refresh();
+    });
+
+    describe('internal', async () => {
+        it('should revert if shares = 0', async () => {
+            await fix.nuggft.connect(accounts.mac).delegate(0);
+
+            await fix.nuggft.connect(accounts.dee).offer(0, { value: toEth('3.000') });
+
+            await Mining.advanceBlockTo(50);
+
+            await fix.nuggft.connect(accounts.dee).claim(0, 0);
+            await fix.nuggft.connect(accounts.mac).claim(0, 0);
+
+            await fix.nuggft.connect(accounts.dee).approve(fix.nuggft.address, 0);
+            await fix.nuggft.connect(accounts.dee).swap(0, toEth('.02000'));
+
+            await fix.nuggft.connect(accounts.frank).mint(2, { value: toEth('20.000') });
+            await fix.nuggft.connect(accounts.frank).commit(0, { value: toEth('20.000') });
+            await fix.nuggft.connect(accounts.dennis).offer(0, { value: toEth('22.000') });
+            await fix.nuggft.connect(accounts.frank).offer(0, { value: toEth('3.000') });
+            await fix.nuggft.connect(accounts.dennis).offer(0, { value: toEth('2.000') });
+            await fix.nuggft.connect(accounts.dennis).offer(0, { value: toEth('2.000') });
+            await fix.nuggft.connect(accounts.charile).offer(0, { value: toEth('55.000') });
+
+            await Mining.advanceBlockTo(250);
+            await Mining.advanceBlock();
+
+            await fix.nuggft.connect(accounts.frank).mint(9, { value: toEth('88') });
+
+            await Mining.advanceBlockTo(350);
+            await fix.nuggft.connect(accounts.frank).claim(9, 9);
+
+            await fix.nuggft.connect(accounts.charile).claim(0, 3);
+
+            const info = await fix.nuggft.infoOf(9);
+            console.log(info.items[0].toString(), accounts.dee.address);
+            await fix.nuggft.connect(accounts.frank).swapItem(9, info.items[0], toEth('14'));
+            const epoch = await fix.nuggft.connect(accounts.charile).epoch();
+
+            await fix.nuggft.connect(accounts.charile).delegateItem(9, info.items[0], 0, { value: toEth('43') });
+
+            await Mining.advanceBlockTo(450);
+
+            console.log('epoch', epoch.toString());
+
+            const info0 = await fix.nuggft.infoOf(0);
+
+            await fix.nuggft.connect(accounts.charile).swapItem(0, info0.items[2], toEth('55'));
+
+            await fix.nuggft.connect(accounts.charile).claimItem(9, info.items[0], 0, epoch.add(2));
+        });
+    });
+});
