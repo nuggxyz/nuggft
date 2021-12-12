67484b5c60c52c6c980c9cce7efcc648abd9d380
dub6ix <90335716+dub6ix@users.noreply.github.com>
2021-12-12 15:33:28 -0600

    move

--- contracts/src/interfaces/IDotNuggHolder.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-interface IDotNuggHolder {
-    function dotNuggUpload(uint256[][] memory items, bytes memory data) external;
-}
--- contracts/src/interfaces/INuggFT.sol
+++ contracts/src/interfaces/INuggFT.sol
@@ -2,51 +2,26 @@
 
 pragma solidity 0.8.4;
 
-interface INuggFT {
-    event PreMint(uint256 tokenId, uint256[] items);
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol';
 
-    event PopItem(uint256 tokenId, uint256 itemId);
+interface ITokenable is IERC721, IERC721Metadata {
+    function proofOf(uint256 tokenId) external view returns (uint256);
 
-    event PushItem(uint256 tokenId, uint256 itemId);
+    function parsedProofOf(uint256 tokenId)
+        external
+        view
+        returns (
+            uint256 proof,
+            uint256[] memory defaultIds,
+            uint256[] memory extraIds,
+            uint256[] memory overrides
+        );
 
-    event Mint(uint256 epoch, address account, uint256 eth);
+    function resolverOf(uint256 tokenId) external view returns (address);
+}
 
-    event Commit(uint256 tokenid, address account, uint256 eth);
-
-    event Offer(uint256 tokenid, address account, uint256 eth);
-
-    event Claim(uint256 tokenid, uint256 endingEpoch, address account);
-
-    event Swap(uint256 tokenid, address account, uint256 eth);
-
-    event CommitItem(uint256 sellingTokenId, uint256 itemId, uint256 buyingTokenId, uint256 eth);
-
-    event OfferItem(uint256 sellingTokenId, uint256 itemId, uint256 buyingTokenId, uint256 eth);
-
-    event ClaimItem(uint256 sellingTokenId, uint256 itemId, uint256 buyingTokenId, uint256 endingEpoch);
-
-    event SwapItem(uint256 sellingTokenId, uint256 itemId, uint256 eth);
-
-    event OpenSlot(uint256 tokenId);
-
-    event Genesis();
-
-    function swapItem(
-        uint256 tokenid,
-        uint256 floor,
-        uint256 itemid
-    ) external;
-
-    function xnugg() external view returns (address payable);
-
-    function dotnugg() external view returns (address);
-
-    function defaultResolver() external view returns (address);
-
-    function genesis() external view returns (uint256 res);
-
-    function epoch() external view returns (uint256 res);
-
+interface ISwapable {
     function delegate(uint256 tokenid) external payable;
 
     function delegateItem(
@@ -84,6 +59,12 @@ interface INuggFT {
 
     function swap(uint256 tokenid, uint256 floor) external;
 
+    function swapItem(
+        uint256 tokenid,
+        uint256 floor,
+        uint256 itemid
+    ) external;
+
     function getOfferByAccount(
         uint256 tokenid,
         uint256 index,
@@ -99,4 +80,40 @@ interface INuggFT {
             uint256 _epoch,
             bool isOwner
         );
+
+    function genesis() external view returns (uint256 res);
+
+    function epoch() external view returns (uint256 res);
+}
+
+interface INuggFT is ISwapable, ITokenable {
+    event PreMint(uint256 tokenId, uint256[] items);
+
+    event PopItem(uint256 tokenId, uint256 itemId);
+
+    event PushItem(uint256 tokenId, uint256 itemId);
+
+    event Mint(uint256 epoch, address account, uint256 eth);
+
+    event Commit(uint256 tokenid, address account, uint256 eth);
+
+    event Offer(uint256 tokenid, address account, uint256 eth);
+
+    event Claim(uint256 tokenid, uint256 endingEpoch, address account);
+
+    event Swap(uint256 tokenid, address account, uint256 eth);
+
+    event CommitItem(uint256 sellingTokenId, uint256 itemId, uint256 buyingTokenId, uint256 eth);
+
+    event OfferItem(uint256 sellingTokenId, uint256 itemId, uint256 buyingTokenId, uint256 eth);
+
+    event ClaimItem(uint256 sellingTokenId, uint256 itemId, uint256 buyingTokenId, uint256 endingEpoch);
+
+    event SwapItem(uint256 sellingTokenId, uint256 itemId, uint256 eth);
+
+    event OpenSlot(uint256 tokenId);
+
+    event Genesis();
+
+    function defaultResolver() external view returns (address);
 }
