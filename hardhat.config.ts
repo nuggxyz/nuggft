238ddc82179c6d53fded32411cad86b28bb26070
dub6ix <90335716+dub6ix@users.noreply.github.com>
2021-12-15 11:41:21 -0600

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -240,7 +240,7 @@ const HardhatConfig: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: '0.8.4',
+                version: '0.8.9',
                 settings: {
                     optimizer: {
                         enabled: true,
