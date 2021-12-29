5d3ee6d7d1ae52afe823a0b00e48548ece4602d0
dub6ix <90335716+dub6ix@users.noreply.github.com>
2021-12-29 08:56:59 -0600

    updates

---
+++ src/_test/utils/DSEmit.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+abstract contract DSEmit {
+    event log(string);
+    event logs(bytes);
+
+    event log_address(address);
+    event log_bytes32(bytes32);
+    event log_int(int256);
+    event log_uint(uint256);
+    event log_bytes(bytes);
+    event log_string(string);
+
+    event log_named_address(string key, address val);
+    event log_named_bytes32(string key, bytes32 val);
+    event log_named_decimal_int(string key, int256 val, uint256 decimals);
+    event log_named_decimal_uint(string key, uint256 val, uint256 decimals);
+    event log_named_int(string key, int256 val);
+    event log_named_uint(string key, uint256 val);
+    event log_named_bytes(string key, bytes val);
+    event log_named_string(string key, string val);
+}
