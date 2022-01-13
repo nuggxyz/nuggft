a6da1e2beab50eae04aec6c6691d051c1eb08a7e
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-01-13 05:05:11 -0600

    adding forge tests

--- src/_deployment/ABC.sol
+++ src/_deployment/ABC.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8.9;
 
+import {DotnuggV1} from '../../../dotnugg-core/src/DotnuggV1.sol';
+
 contract PureDeployerCallback {
     address public dotnuggV1;
 
---
+++ src/_run/InitCodeHash.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+contract InitCodeHash {
+    function run(address nuggftv1, uint160 id)
+        external
+        returns (
+            address eoa__deployer,
+            address contract__deployer,
+            bytes32 hash__nuggft,
+            bytes32 hash__dotnugg
+        )
+    {
+        assembly {
+            let addr := mload(0x40)
+
+            mstore(addr, shl(72, or(shl(176, 0xd6), or(shl(168, 0x94), or(shl(8, caller()), 0x02)))))
+
+            addr := keccak256(addr, 23)
+        }
+
+        // console.log('export FACTORY="${deployerAddress}"');
+        // console.log('export CALLER="${wallet.address}"');
+        // console.log('export INIT_CODE_HASH="${initCodeHash}"');
+
+        // payable(msg.sender).transfer(address(this).balance);
+    }
+}
