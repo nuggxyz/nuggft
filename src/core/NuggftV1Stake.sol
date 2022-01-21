0a23286cffb2e744bf6b81dc7d68884fa7dc62b8
danny7even <90333397+danny7even@users.noreply.github.com>
2022-01-21 01:35:55 +0100

    testing stake

--- src/core/NuggftV1Stake.sol
+++ src/core/NuggftV1Stake.sol
@@ -84,7 +84,7 @@ abstract contract NuggftV1Stake is INuggftV1Stake, NuggftV1Proof {
 
     /// @notice handles the adding of shares - ensures enough eth is being added
     /// @dev this is the only way to add shares - the logic here ensures that "ethPerShare" can never decrease
-    function addStakedShareFromMsgValue(uint96 offset) internal {
+    function addStakedShareFromMsgValue(uint96 offset) internal trackGas {
         uint96 value = msg.value.to96() - offset;
 
         uint256 cache = stake;
