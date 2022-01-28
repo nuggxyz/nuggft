2fdc343fa01fbb5322ddbd2e853351e81c10ca16
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-01-28 12:42:19 -0600

    adding reverts

--- src/_test/core/system/fragments.t.sol
+++ src/_test/core/system/fragments.t.sol
@@ -14,18 +14,18 @@ contract fragments is NuggftV1Test {
             value = 1 ether;
         }
 
-        expect.mint().exec(token, lib.txdata(user, value));
+        expect.mint().exec(token, lib.txdata(user, value, ''));
     }
 
     function deeSellsAnItem() public {
         uint96 value = 1 ether;
 
-        expect.mint().exec(500, lib.txdata(users.dee, 0));
+        expect.mint().exec(500, lib.txdata(users.dee, 0, ''));
 
         (, uint8[] memory ids, , , , ) = nuggft.proofToDotnuggMetadata(500);
 
         itemId = ids[1] | (1 << 8);
 
-        expect.sell().exec(500, itemId, value, lib.txdata(users.dee, 0));
+        expect.sell().exec(500, itemId, value, lib.txdata(users.dee, 0, ''));
     }
 }
--- src/_test/expect/mint.sol
+++ src/_test/expect/mint.sol
@@ -45,8 +45,10 @@ contract expectMint is base {
     function exec(uint160 tokenId, lib.txdata memory txdata) public {
         forge.vm.deal(txdata.from, txdata.from.balance + txdata.value);
         this.start(tokenId, txdata.from, txdata.value);
-        forge.vm.prank(txdata.from);
+        forge.vm.startPrank(txdata.from);
+        if (txdata.str.length > 0) forge.vm.expectRevert(txdata.str);
         nuggft.mint{value: txdata.value}(tokenId);
+        forge.vm.stopPrank();
         this.stop();
     }
 
