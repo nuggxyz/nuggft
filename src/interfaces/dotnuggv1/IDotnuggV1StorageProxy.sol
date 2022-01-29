e5443fd118dd73ed15ead42358544f19e2f040db
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-01-29 08:22:31 -0600

    deployment looks ok now

--- src/interfaces/dotnuggv1/IDotnuggV1StorageProxy.sol
+++ src/interfaces/dotnuggv1/IDotnuggV1StorageProxy.sol
@@ -5,9 +5,11 @@ pragma solidity 0.8.11;
 interface IDotnuggV1StorageProxy {
     function stored(uint8 feature) external view returns (uint8);
 
-    function store(uint8 feature, bytes calldata data) external returns (uint8 amount);
+    function store(uint8 feature, bytes memory data) external returns (uint8 amount);
 
     function unsafeBulkStore(bytes[] calldata data) external;
+
+    function pointers() external view returns (address[][] memory res);
 
     function init(address _implementer) external;
 
