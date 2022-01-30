0e85ad0254842a34d99b5134d9f3238a821ade9f
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-01-30 17:07:27 -0600

    no mo green

---
+++ src/_test/core/revert2/sell/revert__sell__0x77.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.11;
+
+import '../../../NuggftV1.test.sol';
+
+contract revert__sell__0x77 is NuggftV1Test {
+    function setUp() public {
+        reset__revert();
+    }
+
+    function test__revert__sell__0x77__fail__desc() public {
+        assert(false);
+    }
+
+    function test__revert__sell__0x77__pass__desc() public {
+        assert(false);
+    }
+}
