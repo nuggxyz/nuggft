41a71d4551b1fa2d69ffc3a2662ca97ff7df7a39
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-01-29 18:04:00 -0600

    fixes

--- src/_test/NuggftV1.test.sol
+++ src/_test/NuggftV1.test.sol
@@ -190,6 +190,48 @@ contract NuggftV1Test is ForgeTest {
         forge.vm.stopPrank();
     }
 
+    function reset__fork() public {
+        ds.setDsTest(address(this));
+
+        PureDeployer dep = new PureDeployer(0, 0, type(RiggedNuggft).creationCode, type(MockDotnuggV1).creationCode, tmpdata);
+        // dep.init();
+
+        processor = MockDotnuggV1(dep.__dotnugg());
+        nuggft = RiggedNuggft(dep.__nuggft());
+
+        // record.build(nuggft.external__agency__slot());
+
+        _nuggft = address(nuggft);
+
+        expect = new Expect(_nuggft);
+
+        _processor = address(processor);
+
+        _migrator = new MockNuggftV1Migrator();
+
+        users.frank = forge.vm.addr(12);
+        forge.vm.deal(users.frank, 90000 ether);
+
+        users.dee = forge.vm.addr(13);
+        forge.vm.deal(users.dee, 90000 ether);
+
+        users.mac = forge.vm.addr(14);
+        forge.vm.deal(users.mac, 90000 ether);
+
+        users.dennis = forge.vm.addr(15);
+        forge.vm.deal(users.dennis, 90000 ether);
+
+        users.charlie = forge.vm.addr(16);
+        forge.vm.deal(users.charlie, 90000 ether);
+
+        users.safe = forge.vm.addr(17);
+        forge.vm.deal(users.safe, 90000 ether);
+
+        forge.vm.startPrank(0x9B0E2b16F57648C7bAF28EDD7772a815Af266E77);
+        nuggft.setIsTrusted(users.safe, true);
+        forge.vm.stopPrank();
+    }
+
     function jump(uint24 to) public {
         uint256 startblock = nuggft.external__toStartBlock(to);
         forge.vm.roll(startblock);
