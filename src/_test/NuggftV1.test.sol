246543f606f802cdd2bd51c771f3824a1c755457
danny7even <90333397+danny7even@users.noreply.github.com>
2022-01-31 13:58:23 +0100

    added loss

--- src/_test/NuggftV1.test.sol
+++ src/_test/NuggftV1.test.sol
@@ -63,6 +63,10 @@ contract RiggedNuggft is NuggftV1 {
             res := agency.slot
         }
     }
+
+    function external__LOSS() public view returns (uint256 res) {
+        res = LOSS;
+    }
 }
 
 library SafeCast {
