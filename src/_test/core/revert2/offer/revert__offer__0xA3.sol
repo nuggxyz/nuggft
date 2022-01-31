4b2f781ccfa84c5213e1ec4e392af12b415bf3c6
danny7even <90333397+danny7even@users.noreply.github.com>
2022-01-31 19:03:57 +0100

    a3

--- src/_test/core/revert2/offer/revert__offer__0xA3.sol
+++ src/_test/core/revert2/offer/revert__offer__0xA3.sol
@@ -10,10 +10,36 @@ contract revert__offer__0xA3 is NuggftV1Test {
     }
 
     function test__revert__offer__0xA3__fail__desc() public {
-        assert(false);
+        (uint160 tokenId, , uint16 itemId, uint96 floor) = scenario_dee_has_sold_an_item();
+
+        expect.mint().from(users.charlie).value(1.1 ether).exec(1234);
+
+        expect.sell().from(users.charlie).exec(1234, 1.2 ether);
+
+        expect.offer().from(users.mac).value(1.3 ether).exec(1234);
+
+        uint96 value = floor + 1 ether;
+
+        expect.offer().err(0xA3).from(users.mac).exec{value: value}(1234, tokenId, itemId);
     }
 
     function test__revert__offer__0xA3__pass__desc() public {
-        assert(false);
+        jump(3000);
+
+        (uint160 tokenId, , uint16 itemId, uint96 floor) = scenario_dee_has_sold_an_item();
+
+        expect.mint().from(users.charlie).value(1.1 ether).exec(1234);
+
+        expect.sell().from(users.charlie).exec(1234, 1.2 ether);
+
+        expect.offer().from(users.mac).value(1.3 ether).exec(1234);
+
+        jump(3003);
+
+        expect.claim().from(users.mac).exec(lib.sarr160(1234), lib.sarrAddress(users.mac));
+
+        uint96 value = floor + 1 ether;
+
+        expect.offer().from(users.mac).exec{value: value}(1234, tokenId, itemId);
     }
 }
