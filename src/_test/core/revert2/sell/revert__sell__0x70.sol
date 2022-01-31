03fd26726fb4d6d0204b956920043abb0acf7aec
danny7even <90333397+danny7even@users.noreply.github.com>
2022-01-31 20:06:16 +0100

    sell

--- src/_test/core/revert2/sell/revert__sell__0x70.sol
+++ src/_test/core/revert2/sell/revert__sell__0x70.sol
@@ -10,10 +10,18 @@ contract revert__sell__0x70 is NuggftV1Test {
     }
 
     function test__revert__sell__0x70__fail__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).value(1 ether).exec(500);
+
+        uint96 value = nuggft.eps() - 10 gwei;
+
+        expect.sell().from(users.frank).err(0x70).exec(500, value);
     }
 
     function test__revert__sell__0x70__pass__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).value(1 ether).exec(500);
+
+        uint96 value = nuggft.eps();
+
+        expect.sell().from(users.frank).exec(500, value);
     }
 }
