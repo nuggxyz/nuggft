e683fb7a00fb82c828825ee4997ac83653d46d6d
danny7even <90333397+danny7even@users.noreply.github.com>
2022-02-02 00:17:44 +0100

    liquid

--- src/_test/core/revert2/liquidate/revert__liquidate__0xA7.sol
+++ src/_test/core/revert2/liquidate/revert__liquidate__0xA7.sol
@@ -10,10 +10,22 @@ contract revert__liquidate__0xA7 is NuggftV1Test {
     }
 
     function test__revert__liquidate__0xA7__fail__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).value(1 ether).exec(500);
+
+        expect.loan().from(users.frank).exec(lib.sarr160(500));
+
+        uint96[] memory value = nuggft.vfl(lib.sarr160(500));
+
+        expect.liquidate().from(users.frank).value(value[0] - 1 gwei).err(0xA7).exec(500);
     }
 
     function test__revert__liquidate__0xA7__pass__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).value(1 ether).exec(500);
+
+        expect.loan().from(users.frank).exec(lib.sarr160(500));
+
+        uint96[] memory value = nuggft.vfl(lib.sarr160(500));
+
+        expect.liquidate().from(users.frank).value(value[0]).exec(500);
     }
 }
--- src/_test/core/revert2/loan/revert__loan__0xA1.sol
+++ src/_test/core/revert2/loan/revert__loan__0xA1.sol
@@ -10,10 +10,14 @@ contract revert__loan__0xA1 is NuggftV1Test {
     }
 
     function test__revert__loan__0xA1__fail__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).value(1 ether).exec(500);
+
+        expect.loan().from(users.mac).err(0xA1).exec(lib.sarr160(500));
     }
 
     function test__revert__loan__0xA1__pass__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).value(1 ether).exec(500);
+
+        expect.loan().from(users.frank).exec(lib.sarr160(500));
     }
 }
