1796898ca0c79b5f4ebdf458377d7676aa4dde45
danny7even <90333397+danny7even@users.noreply.github.com>
2022-02-01 22:48:14 +0100

    mint tests

--- src/_test/core/revert2/mint/revert__mint__0x65.sol
+++ src/_test/core/revert2/mint/revert__mint__0x65.sol
@@ -10,10 +10,14 @@ contract revert__mint__0x65 is NuggftV1Test {
     }
 
     function test__revert__mint__0x65__fail__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).err(0x65).exec(499);
+
+        expect.mint().from(users.frank).err(0x65).exec(10501);
     }
 
     function test__revert__mint__0x65__pass__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).exec(500);
+
+        expect.mint().from(users.frank).exec(10499);
     }
 }
