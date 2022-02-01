b2c59018ed5b3ec71a7ba064f645bf6429dbdef9
danny7even <90333397+danny7even@users.noreply.github.com>
2022-02-02 00:17:44 +0100

    liquid

--- src/_test/core/revert2/liquidate/revert__liquidate__0xA8.sol
+++ src/_test/core/revert2/liquidate/revert__liquidate__0xA8.sol
@@ -10,10 +10,20 @@ contract revert__liquidate__0xA8 is NuggftV1Test {
     }
 
     function test__revert__liquidate__0xA8__fail__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).value(.5 ether).exec(500);
+
+        // expec called debt() which performed this check earlier than desired
+        forge.vm.deal(users.frank, 900 ether);
+        forge.vm.prank(users.frank);
+        forge.vm.expectRevert(hex'A8');
+        nuggft.liquidate{value: 1 ether}(500);
     }
 
     function test__revert__liquidate__0xA8__pass__desc() public {
-        assert(false);
+        expect.mint().from(users.frank).value(.5 ether).exec(500);
+
+        expect.loan().from(users.frank).exec(lib.sarr160(500));
+
+        expect.liquidate().from(users.frank).value(1 ether).exec(500);
     }
 }
