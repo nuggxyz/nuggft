4e03cd64fe347f73e00e5ae74167d349ba3d195a
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-03-10 12:21:15 -0600

    0x76 revert tests

--- src/_test/core/revert2/claim/revert__claim__0x76.sol
+++ src/_test/core/revert2/claim/revert__claim__0x76.sol
@@ -2,14 +2,34 @@
 
 pragma solidity 0.8.12;
 
-import '../../../NuggftV1.test.sol';
+import "../../../NuggftV1.test.sol";
 
 abstract contract revert__claim__0x76 is NuggftV1Test {
     function test__revert__claim__0x76__fail__desc() public {
-        assert(false);
+        jump(3000);
+
+        uint160 tokenId = nuggft.epoch();
+
+        expect.offer().from(users.frank).exec(tokenId);
+
+        expect.offer().from(users.dee).exec{value: 43 ether}(tokenId);
+
+        jump(3001);
+
+        expect.claim().from(users.frank).err(0x76).exec(array.b160(tokenId), array.bAddress(users.frank, users.dee));
     }
 
     function test__revert__claim__0x76__pass__desc() public {
-        assert(false);
+        jump(3000);
+
+        uint160 tokenId = nuggft.epoch();
+
+        expect.offer().from(users.frank).exec(tokenId);
+
+        expect.offer().from(users.dee).exec{value: 43 ether}(tokenId);
+
+        jump(3001);
+
+        expect.claim().from(users.frank).exec(array.b160(tokenId), array.bAddress(users.frank));
     }
 }
