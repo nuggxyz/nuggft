fccd77b3453edbda3c43503c5b36cfd1b18358f3
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-03-10 12:34:27 -0600

    0xA5 revert tests

--- src/_test/core/revert2/claim/revert__claim__0xA5.sol
+++ src/_test/core/revert2/claim/revert__claim__0xA5.sol
@@ -2,14 +2,30 @@
 
 pragma solidity 0.8.12;
 
-import '../../../NuggftV1.test.sol';
+import "../../../NuggftV1.test.sol";
 
 abstract contract revert__claim__0xA5 is NuggftV1Test {
     function test__revert__claim__0xA5__fail__desc() public {
-        assert(false);
+        jump(OFFSET);
+
+        uint160 tokenId = nuggft.epoch();
+
+        expect.offer().from(users.dee).exec(tokenId);
+
+        jump(OFFSET + 1);
+
+        expect.claim().from(users.frank).err(0xA5).exec(array.b160(tokenId), array.bAddress(users.frank));
     }
 
     function test__revert__claim__0xA5__pass__desc() public {
-        assert(false);
+        jump(OFFSET);
+
+        uint160 tokenId = nuggft.epoch();
+
+        expect.offer().from(users.dee).exec(tokenId);
+
+        jump(OFFSET + 1);
+
+        expect.claim().from(users.dee).exec(array.b160(tokenId), array.bAddress(users.dee));
     }
 }
