06ed38f09a09843bc881462d3dd068aabd536679
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-03-26 12:43:18 -0500

    before roll fixes

--- src/_test/core/revert2/rebalance/revert__rebalance__0xAA.sol
+++ src/_test/core/revert2/rebalance/revert__rebalance__0xAA.sol
@@ -5,19 +5,21 @@ pragma solidity 0.8.13;
 import "../../../NuggftV1.test.sol";
 
 abstract contract revert__rebalance__0xAA is NuggftV1Test {
+    uint160 private TOKEN1 = mintable(0);
+
     function test__revert__rebalance__0xAA__fail__desc() public {
-        expect.mint().from(users.frank).value(20 ether).exec(500);
+        expect.mint().from(users.frank).value(20 ether).exec(TOKEN1);
 
-        expect.loan().from(users.frank).exec(lib.sarr160(500));
+        expect.loan().from(users.frank).exec(lib.sarr160(TOKEN1));
 
-        expect.rebalance().from(users.frank).value(.1 ether).err(0xAA).exec(lib.sarr160(500));
+        expect.rebalance().from(users.frank).value(nuggft.vfr(lib.sarr160(TOKEN1))[0] - 1).err(0xAA).exec(lib.sarr160(TOKEN1));
     }
 
     function test__revert__rebalance__0xAA__pass__desc() public {
-        expect.mint().from(users.frank).value(20 ether).exec(500);
+        expect.mint().from(users.frank).value(20 ether).exec(TOKEN1);
 
-        expect.loan().from(users.frank).exec(lib.sarr160(500));
+        expect.loan().from(users.frank).exec(lib.sarr160(TOKEN1));
 
-        expect.rebalance().from(users.frank).value(nuggft.vfr(lib.sarr160(500))[0]).exec(lib.sarr160(500));
+        expect.rebalance().from(users.frank).value(nuggft.vfr(lib.sarr160(TOKEN1))[0]).exec(lib.sarr160(TOKEN1));
     }
 }
