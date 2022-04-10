56fb70499a5d75cb523c7c106b33f8eb71cc8ebe
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-04-10 13:09:12 -0500

    implement dotnugg strip down and fix broken tests

--- src/core/NuggftV1Proof.sol
+++ src/core/NuggftV1Proof.sol
@@ -6,91 +6,12 @@ import {INuggftV1Proof} from "../interfaces/nuggftv1/INuggftV1Proof.sol";
 import {IDotnuggV1Safe} from "../interfaces/dotnugg/IDotnuggV1Safe.sol";
 import {IERC1155, IERC165} from "../interfaces/IERC721.sol";
 import {INuggftV1} from "../interfaces/nuggftv1/INuggftV1.sol";
+import {NuggftV1Items} from "../NuggftV1Items.sol";
 
-import {DotnuggV1Lib} from "../libraries/DotnuggV1Lib.sol";
+import {DotnuggV1Lib, parseItemIdAsString} from "../libraries/DotnuggV1Lib.sol";
 
 import {NuggftV1Epoch} from "./NuggftV1Epoch.sol";
 import {NuggftV1Trust} from "./NuggftV1Trust.sol";
-
-// 0xe5910fca
-contract Erc1155Emmitter is IERC1155 {
-    INuggftV1 immutable nuggftv1;
-
-    bytes32 constant Event__TransferItem = 0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62;
-
-    constructor() {
-        nuggftv1 = INuggftV1(msg.sender);
-    }
-
-    fallback() external {
-        address nuggft = address(nuggftv1);
-        assembly {
-            if iszero(eq(caller(), nuggft)) {
-                revert(0x00, 0x00)
-            }
-
-            let work := calldataload(0x00)
-
-            let operator := and(work, 0xffffff)
-
-            mstore(0x00, and(shr(24, work), 0xffff))
-            mstore(0x20, 1)
-
-            let from := 0
-            let to := 0
-
-            switch shr(40, work)
-            case 1 {
-                from := nuggft
-                to := address()
-            }
-            case 2 {
-                from := address()
-                to := nuggft
-            }
-            case 3 {
-                from := nuggft
-                to := 0
-            }
-            default {
-                from := address()
-                to := nuggft
-            }
-
-            log4(0x00, 0x40, Event__TransferItem, operator, from, to)
-        }
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return
-            interfaceId == 0xd9b67a26 || //
-            interfaceId == 0x0e89341c ||
-            interfaceId == type(IERC165).interfaceId;
-    }
-
-    function name() public pure returns (string memory) {
-        return "Nugg Fungible Token V1 (Items)";
-    }
-
-    function symbol() public pure returns (string memory) {
-        return "NUGGFT-ITEM";
-    }
-
-    function uri(uint256 tokenId) public view virtual override returns (string memory res) {
-        // prettier-ignore
-        res = string(
-            nuggftv1.dotnuggV1().encodeJsonAsBase64(
-                abi.encodePacked(
-                     '{"name":"',         name(),
-                    '","description":"',  nuggftv1.dotnuggV1().parseItemIdAsString(uint16(tokenId),
-                            ["base", "eyes", "mouth", "hair", "hat", "background", "scarf", "held"]),
-                    '","image":"',        nuggftv1.itemURI(tokenId),
-                    '}'
-                )
-            )
-        );
-    }
-}
 
 abstract contract NuggftV1Proof is INuggftV1Proof, NuggftV1Epoch, NuggftV1Trust {
     mapping(uint160 => uint256) proofs;
@@ -100,7 +21,7 @@ abstract contract NuggftV1Proof is INuggftV1Proof, NuggftV1Epoch, NuggftV1Trust 
 
     IDotnuggV1Safe public immutable override dotnuggV1;
 
-    Erc1155Emmitter public immutable emitter;
+    NuggftV1Items public immutable emitter;
 
     constructor(address dotnugg) {
         // address res;
@@ -116,7 +37,7 @@ abstract contract NuggftV1Proof is INuggftV1Proof, NuggftV1Epoch, NuggftV1Trust 
 
         // firse index of sender
         dotnuggV1 = IDotnuggV1Safe(dotnugg);
-        emitter = new Erc1155Emmitter();
+        emitter = new NuggftV1Items();
     }
 
     /// @inheritdoc INuggftV1Proof
--- src/interfaces/dotnugg/IDotnuggV1Resolver.sol
+++ src/interfaces/dotnugg/IDotnuggV1Resolver.sol
@@ -7,13 +7,7 @@ interface IDotnuggV1Resolver {
 
     function combo(uint256[][] memory reads, bool base64) external view returns (string memory data);
 
-    function calc(uint256[] memory read) external view returns (uint256[] memory calculated);
-
-    function combo(uint256[] memory read, bool base64) external view returns (string memory data);
-
     function svg(uint256[] memory calculated, bool base64) external view returns (string memory data);
-
-    function svg(uint256[] memory calculated) external view returns (string memory data);
 
     function encodeJsonAsBase64(bytes memory input) external pure returns (bytes memory data);
 
@@ -23,15 +17,15 @@ interface IDotnuggV1Resolver {
 
     function encodeSvgAsUtf8(bytes memory input) external pure returns (bytes memory data);
 
-    function props(uint8[8] memory ids, string[8] memory labels) external pure returns (string memory);
+    function props(uint8[8] memory input, string[8] memory labels) external pure returns (string memory);
 
-    function props(uint256 proof, string[8] memory labels) external pure returns (string memory);
+    // function decodeProof(uint256 input) external pure returns (uint16[16] memory res);
 
-    function decodeProof(uint256 input) external pure returns (uint16[16] memory res);
+    // function encodeProof(uint8[8] memory ids) external pure returns (uint256 proof);
 
-    function encodeProof(uint8[8] memory ids) external pure returns (uint256 proof);
+    // function encodeProof(uint16[16] memory ids) external pure returns (uint256 proof);
 
-    function encodeProof(uint16[16] memory ids) external pure returns (uint256 proof);
+    // function decodeProofCore(uint256 proof) external pure returns (uint8[8] memory res);
 
-    function parseItemIdAsString(uint16 itemId, string[8] memory labels) external pure returns (string memory);
+    // function parseItemIdAsString(uint16 itemId, string[8] memory labels) external pure returns (string memory);
 }
