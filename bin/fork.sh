09886118e263c52a235de1184772ed9bea8fd554
dub6ix <90335716+dub6ix@users.noreply.github.com>
2023-03-12 10:14:33 -0500

    format for some reason

--- bin/fork.sh
+++ bin/fork.sh
@@ -6,11 +6,11 @@ GAS=${2:-29999999}
 
 if [ "$NETWORK" == "local" ]; then
 	# ETH_RPC_URL="http://127.0.0.1:8545"
-	anvil --mnemonic "$MNEMONIC_PATH_1"
+	export HTTP_PROXY="http://127.0.0.1:8889"
+	anvil --mnemonic "$MNEMONIC_PATH_1" --rpc-url "$ETH_RPC_URL" --steps-tracing
 
 else
+	anvil --mnemonic "$MNEMONIC_PATH_1" --rpc-url "$ETH_RPC_URL" --silent --gas-limit "$GAS"
 
-	ETH_RPC_URL="https://$NETWORK.infura.io/v3/$INFURA_KEY"
+	# ETH_RPC_URL="https://$NETWORK.infura.io/v3/$INFURA_KEY"
 fi
-
-anvil --mnemonic "$MNEMONIC_PATH_1" --rpc-url "$ETH_RPC_URL" --silent --gas-limit "$GAS"
